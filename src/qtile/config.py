9fb65e50c9d96794cb61947ecf51f209eff13bf9
Dimitar Popchev <dpopchev@gmail.com>
2024-02-13 23:58:56 +0200

    manually kill xxkb at evry start to pop in systray

--- src/qtile/config.py
+++ src/qtile/config.py
@@ -51,9 +51,9 @@ def start_color_adjustment_service(utils: Path = UTILS):
     subprocess.Popen([util], shell=False)
 
 @hook.subscribe.startup
-def xxkb(utils: Path = UTILS):
-    subprocess.run(['killall', whoami()])
-    subprocess.Popen([whoami()], shell=False)
+def xxkb():
+    subprocess.run(['killall', '-9', whoami()])
+    subprocess.Popen([whoami()])
 
 @hook.subscribe.startup_once
 def mute_volume(utils: Path = UTILS):
@@ -172,21 +172,12 @@ screens = [
                 widget.CurrentLayout(),
                 widget.GroupBox(hide_unused=True),
                 widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
-                widget.Systray(),
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.Systray(),
             ],
-            24,
+            20,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
